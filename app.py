e54cac8cc8e2b7ff3a1a03b71419f4156295db4b
parth-vader <vermaparth97@gmail.com>
2018-10-26 18:51:32 +0530

    final update

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from azure.cognitiveservices.vision.customvision.prediction.prediction_endpoint 
 import os
 from werkzeug.utils import secure_filename
 import shutil
-
+from twilio.rest import Client
 app = Flask(__name__,static_url_path='/static')
 
 # For Custom Vision
@@ -32,7 +32,17 @@ def dashboard():
 
 @app.route('/maps')
 def maps():
-    return render_template('maps.html')
+    arr = []
+    # staticpath = os.path.join(APP_ROOT,'/static')
+    staticpath = APP_ROOT + "/static"
+    print(staticpath)
+    for file in os.listdir(staticpath):
+        print(file)
+        if file.endswith(".jpg"):
+            print(file)
+            arr.append(file)
+
+    return render_template('maps.html',arr=arr)
 
 @app.route("/upload")
 def upload():
@@ -90,7 +100,59 @@ def gettweetimg():
 
 @app.route('/chat')
 def chat():
+
     return render_template('template.html')
+
+@app.route('/chatsend', methods=['POST'])
+def send():
+    body = "Helpline: \n"
+    body += request.form.get('message',None)
+    account_sid = 'ACd3fdc744b19638b70804fa820461bb1e'
+    auth_token = '3b519ca887977de2eaad3fd0001b7969'
+    client = Client(account_sid, auth_token)
+
+    message = client.messages \
+        .create(
+            body=body,
+            from_='+14807507605',
+            to='+919800332777'
+        )
+    
+    aa = client.messages.list(from_='+919800332777')
+    
+    loc1 = getlatlon()
+    lat  = loc1[0]
+    lon = loc1[1]
+    
+    msg1 = """ 
+    SOS. Phone no.:9800332777\n
+    
+    Location = {0} {1}\n
+    
+    {2} \n""".format(lat,lon,aa[0].body)
+    
+    loc2 = getlatlon()
+    lat  = loc2[0]
+    lon = loc2[1]
+    msg2 = """
+    SOS. Phone no.:9800332777\n
+
+    Location = {0} {1}\n
+    
+    {2} \n""".format(lat,lon,aa[1].body)
+    
+
+    return render_template('template.html',msg1=msg1,msg2=msg2,content=body)
+
+def getlatlon():
+    lat1 = 20.5
+    lat2 = 22.5
+    lon1 = 82.5
+    lon2 = 87.5
+    lat = random.uniform(lat1,lat2)
+    lon = random.uniform(lon1,lon2)
+    
+    return [round(lat,1),round(lon,1)]
 
 if __name__ == "__main__":
     app.run()
